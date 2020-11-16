31d4abf68d7bf6599a8cd786ea4919f9d47142e2
Niko Bentley <niko@maplestorm.ca>
2020-11-16 12:07:53 -0400

    added picture of me and removed default gatsby images

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import styled from "styled-components";
+import Img from "gatsby-image";
+import { graphql } from "gatsby";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
@@ -12,27 +14,68 @@ const HeroContainer = styled.div`
   display: flex;
   justify-content: center;
 
-  .hero-content {
+  .hero {
     max-width: 1200px;
     width: 80%;
     padding: 75px 0;
+
+    & > div {
+      max-width: 45%;
+    }
+
+    &-image {
+      .me {
+        border-radius: 250px;
+      }
+    }
   }
 `;
 
-const IndexPage = () => (
+export const pageQuery = graphql`
+  query {
+    file(relativePath: { eq: "me.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 500) {
+          base64
+          tracedSVG
+          aspectRatio
+          src
+          srcSet
+          srcWebp
+          srcSetWebp
+          sizes
+          originalImg
+          originalName
+        }
+      }
+    }
+  }
+`;
+
+const IndexPage = ({ data }) => (
   <Layout>
+    {console.log(data)}
     <SEO title="Home" />
     <HeroContainer>
-      <div className="hero-content">
-        <h1>
-          Hi, Niko here. <br />
-          Full-Stack Web Developer with a passion for learning <br />
-          based in Nova Scotia,&nbsp;
-          <span style={{ color: COLOURS.redSalsa, fontSize: "2rem" }}>
-            Canada
-          </span>
-          .
-        </h1>
+      <div className="hero">
+        <div className="hero-text">
+          <h1>
+            Hi, Niko here. <br />
+            Full-Stack Web Developer with a passion for learning <br />
+            based in Nova Scotia,&nbsp;
+            <span style={{ color: COLOURS.redSalsa, fontSize: "2rem" }}>
+              Canada
+            </span>
+            .
+          </h1>
+        </div>
+        <div className="hero-image">
+          <Img
+            className="me"
+            fluid={data.file.childImageSharp.fluid}
+            alt="A picture of me at the beach in São Jacinto, Portugal."
+          />
+        </div>
       </div>
     </HeroContainer>
   </Layout>
