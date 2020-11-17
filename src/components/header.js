5a73cf4e85926106ca70dff82e64f6cc6438fa30
Niko Bentley <niko@maplestorm.ca>
2020-11-16 21:33:51 -0400

    added react-icons to the project, and added social links to header

--- src/components/header.js
+++ src/components/header.js
@@ -2,6 +2,7 @@ import { Link } from "gatsby";
 import PropTypes from "prop-types";
 import React from "react";
 import styled from "styled-components";
+import { AiFillTwitterCircle, AiFillGithub } from "react-icons/ai";
 
 import { COLOURS } from "./styles";
 
@@ -30,15 +31,44 @@ const HeaderContainer = styled.div`
   }
 `;
 
-const Header = ({ siteTitle }) => (
-  <HeaderWrapper>
-    <HeaderContainer>
-      <div className="home-link">
-        <Link to="/">{siteTitle}</Link>
-      </div>
-    </HeaderContainer>
-  </HeaderWrapper>
-);
+const SocialLinks = styled.div`
+  display: inline-grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 10px;
+
+  a {
+    color: ${COLOURS.fontColour};
+    font-size: 2rem;
+
+    &:hover,
+    &:focus {
+      color: ${COLOURS.redSalsa};
+    }
+  }
+`;
+
+const Header = ({ siteTitle }) => {
+  const twitterLink = "https://twitter.com/Niko_Bentley";
+  const githubLink = "https://github.com/MapleThunder";
+
+  return (
+    <HeaderWrapper>
+      <HeaderContainer>
+        <div className="home-link">
+          <Link to="/">{siteTitle}</Link>
+        </div>
+        <SocialLinks>
+          <Link to={twitterLink}>
+            <AiFillTwitterCircle />
+          </Link>
+          <Link to={githubLink}>
+            <AiFillGithub />
+          </Link>
+        </SocialLinks>
+      </HeaderContainer>
+    </HeaderWrapper>
+  );
+};
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
