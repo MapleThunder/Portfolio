a427555ce132503e8323298e4357295cbde0a566
Niko Bentley <niko@maplestorm.ca>
2020-11-25 18:12:52 -0400

    added section components

---
+++ src/components/section.js
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledSection = styled.section`
+  margin-top: 2rem;
+`;
+
+const Section = ({ content }) => {
+  const { frontmatter, html } = content;
+
+  return (
+    <StyledSection>
+      <h2>{frontmatter.title}</h2>
+      <p dangerouslySetInnerHTML={{ __html: html }}></p>
+    </StyledSection>
+  );
+};
+
+export default Section;
