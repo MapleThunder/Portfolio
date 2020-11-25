9249ea8b7ebd03f01a5fee0cb7e839e6ff2fe866
Niko Bentley <niko@maplestorm.ca>
2020-11-25 18:13:30 -0400

    updated styles for layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,3 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
-
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
@@ -12,31 +5,16 @@ import styled from "styled-components";
 import "../styles/normalize.css";
 import "../styles/main.css";
 import "../styles/style.css";
-import { COLOURS } from "../styles";
 
-const StyledLayout = styled.div`
-  width: 100%;
-  min-height: 100vh;
-  margin: 0 auto;
-  display: grid;
-  grid-template-rows: auto 1fr auto;
-  grid-template-columns: 100%;
-  background-color: ${COLOURS.background};
-  color: ${COLOURS.fontColour};
-
-  #main-content {
-    width: 100%;
-    max-width: 62.5rem;
-    margin: 0 auto;
-    padding: 0 2.5rem;
-  }
+const Main = styled.main`
+  padding: calc(4rem + var(--headerHeight)) var(--containerPadding);
 `;
 
 const Layout = ({ children }) => {
   return (
-    <main id="main-content" className="layout">
+    <Main id="main-content" className="layout">
       {children}
-    </main>
+    </Main>
   );
 };
 
