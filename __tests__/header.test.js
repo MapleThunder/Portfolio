d49f902da2a3b35085984114d4cba2f5b22bb24e
Niko Bentley <niko@maplestorm.ca>
2020-12-20 15:23:03 -0400

    initial test files

---
+++ __tests__/header.test.js
@@ -0,0 +1,10 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import Header from "../src/components/header";
+
+describe("Header", () => {
+  it("renders correctly", () => {
+    const tree = renderer.create(<Header siteTitle="NB" />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
---
+++ __tests__/hero.test.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { StaticQuery } from "gatsby";
+import { PureHero as Hero } from "../src/components/hero";
+
+// beforeEach(() => {
+//   StaticQuery.mockImplementationOnce(({ render }) =>
+//     render({
+
+//     })
+//   );
+// });
+
+describe("Hero", () =>
+  it("renders correctly", () => {
+    const node = {
+      frontmatter: {
+        emoji: "👋",
+        greetings: "Ola",
+        subtitleHighlight: "things for the web",
+        subtitlePrefix: "I build and design ",
+        title: "I'm Niko Bentley",
+      },
+      html:
+        "<p>Web Developer with a passion for learning.<br>\nBased in Halifax, Nova Scotia.</p>",
+    };
+    const image = {
+      childImageSharp: {
+        fluid: {
+          src: "/static/c392e3865190c633b0435677eecb6f43/6ed45/me.jpg",
+          srcWebp: "/static/c392e3865190c633b0435677eecb6f43/90b9d/me.webp",
+          srcSetWebp:
+            "/static/c392e3865190c633b0435677eecb6f43/f5db0/me.webp 525w,\n/static/c392e3865190c633b0435677eecb6f43/565e9/me.webp 1050w,\n/static/c392e3865190c633b0435677eecb6f43/90b9d/me.webp 2100w",
+          sizes: "(max-width: 2100px) 100vw, 2100px",
+        },
+      },
+    };
+    const { getByTestId } = render(<Hero node={node} image={image} />);
+
+    expect(getByTestId("hero-title")).toHaveTextContent("I'm Niko Bentley");
+  }));
